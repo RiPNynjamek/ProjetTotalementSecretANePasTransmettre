5ca33f5ad62075010a8cb7a1b2d61d63b8abb9d7
cyril-corbon <cyril.corbon@viacesi.fr>
2017-06-26 17:28:29 +0200

    comunication work

--- J2E/connectionPoc/src/main/java/com/gen/connectionpoc/connection.java
+++ J2E/connectionPoc/src/main/java/com/gen/connectionpoc/connection.java
@@ -15,7 +15,7 @@ import java.lang.Thread;
 
 public class connection {
     
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "decrypt";
     public void send()
     {
         try{
@@ -51,15 +51,14 @@ public class connection {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
             System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
             Consumer consumer = new DefaultConsumer(channel) {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope,
-                             AMQP.BasicProperties properties, byte[] body)
-            throws IOException {
-            String message = new String(body, "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
-            }
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+                    System.out.println(" [x] Received '" + message + "'");
+                }
             };
-channel.basicConsume(QUEUE_NAME, true, consumer);
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+            System.in.read();
         }catch(Exception e){
             
         }
