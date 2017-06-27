abbbc802103272471fefdf782a232a0f2e035691
cyril-corbon <cyril.corbon@viacesi.fr>
2017-06-27 13:34:28 +0200

    create ejbPattern and load it on another domain

---
+++ J2E/pocConnection/src/main/java/com/gen/pocconnection/main/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gen.pocconnection.main;
+
+import com.gen.pocconnection.pattern.*;
+import com.gen.pocconnection.connection;
+
+/**
+ *
+ * @author Cyril
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        patternMail mail = new patternMail();
+        mail.searchPattern("{Message : toto}");
+    }
+    
+}
---
+++ J2E/pocConnection/src/main/java/com/gen/pocconnection/pattern/patternMail.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gen.pocconnection.pattern;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Cyril
+ */
+public class patternMail implements iPattern{
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX = 
+    Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", Pattern.CASE_INSENSITIVE);
+    private String object;
+    private String message;
+    @Override
+    public void searchPattern(String object) {
+        System.out.println("begin pattern detection");
+        System.out.println(object);
+        JSONObject json = new JSONObject(object);
+        System.out.println(json);
+        message = json.getString("Message");
+        //System.out.println(message);
+        if(message.toLowerCase().contains("@")){
+            Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(message);
+            if(matcher.find()){
+                System.out.println(message +" est une adresse mail");
+            }else{
+                System.out.println(message +" n'est pas une adresse mail");
+            }
+        }else{
+            System.out.println("not an email");
+        }
+    }
+    
+
+}
