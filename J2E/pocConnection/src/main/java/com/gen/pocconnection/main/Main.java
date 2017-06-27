8d5a5f02ea5c1c4a9c6f8da12cb94147f45d0523
RiPNynjamek <adrien.jaussaud@viacesi.fr>
2017-06-27 16:57:02 +0200

    Revert "ejb pattern model fonction"
    This reverts commit 5228c309c5447fe8935674dd59611e3da85c1690.

--- J2E/pocConnection/src/main/java/com/gen/pocconnection/main/Main.java
+++ J2E/pocConnection/src/main/java/com/gen/pocconnection/main/Main.java
@@ -7,7 +7,7 @@ package com.gen.pocconnection.main;
 
 import com.gen.pocconnection.pattern.*;
 import com.gen.pocconnection.connection;
-import com.gen.ejbpattern.patternDico;
+
 /**
  *
  * @author Cyril
@@ -20,8 +20,8 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        patternDico dico = new patternDico();
-        dico.searchPattern("{Message : maison toto@gmail.com}");
+        patternMail mail = new patternMail();
+        mail.searchPattern("{Message : toto}");
     }
     
 }
